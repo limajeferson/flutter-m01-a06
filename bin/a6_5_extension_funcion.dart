f1c0ebf43cd115867c6d8f03b084f677597c8d83
limajeferson <jefersonlima@outlook.com>
2022-01-13 12:24:41 -0300

    20220110_aula05

--- bin/a6_5_extension_funcion.dart
+++ bin/a6_5_extension_funcion.dart
@@ -1,16 +1,27 @@
 import 'dart:io';
 
-//*
+//* Estrutura - extension + on | this -> current instance
+extension Calculadora on double {
+  double plus(double other) => this + other;
+  double minus(double other) => this - other;
+  double times(double other) => this * other;
+  double dividedBy(double other) => this / other;
+  int integerPart() => this ~/ 1;
+  double decimalPart() => this % 1;
+}
 
-//*
+//* Exemplo
 void main() {
-  // final inputs = readFromKeyboard(stdin.readLineSync() ?? '');
-  // final number1 = double.parse(inputs[0]);
-  // final operator = inputs[1];
-  // final number2 = double.parse(inputs[2]);
-  // final result = calculate(number1, operator, number2);
-  // final msg = formatMessage(result);
-  // print(msg);
+  // final name = 'Jeferson';
+  // name.toUpperCase();
+
+  final inputs = readFromKeyboard(stdin.readLineSync() ?? '');
+  final number1 = double.parse(inputs[0]);
+  final operator = inputs[1];
+  final number2 = double.parse(inputs[2]);
+  final result = calculate(number1, operator, number2);
+  final msg = formatMessage(result);
+  print(msg);
 }
 
 List<String> readFromKeyboard(String line) {
@@ -21,20 +32,20 @@ List<String> readFromKeyboard(String line) {
 double calculate(double number1, String operator, double number2) {
   switch (operator) {
     case '-':
-      return number1 - number2;
+      return number1.minus(number2);
     case '*':
-      return number1 * number2;
+      return number1.times(number2);
     case '/':
-      return number1 / number2;
+      return number1.dividedBy(number2);
     default:
-      return number1 + number2;
+      return number1.plus(number2);
   }
 }
 
 String formatMessage(double result) {
   final resultAprox = result.toStringAsFixed(5);
-  final resultInteiro = result ~/ 1;
-  final resultResto = result % 1;
+  final resultInteiro = result.integerPart();
+  final resultResto = result.decimalPart();
 
   return resultResto > 0
       ? double.parse(resultAprox).toString()
